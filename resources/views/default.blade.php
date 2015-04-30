21b93fac2e121286d1f6cb96ea491a57d7190a48
fzed51 <fzed51@users.noreply.github.com>
2015-04-30 16:43:58 +0200

    :up: commit temporaire

--- resources/views/default.blade.php
+++ resources/views/default.blade.php
@@ -35,6 +35,9 @@
 		<div class="container">
 			@yield('content')
 		</div><!-- /.container -->
+		<!-- jQuery -->
+		<script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+		<!-- Bootstrap js-->
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
 	</body>
 </html>
