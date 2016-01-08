30f0af4d116f23af73d5e739cbd9d54bd272f9e9
fzed51 <fzed51@users.noreply.github.com>
2016-01-08 02:42:42 +0100

    :up: début de test pour le htaccess

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+echo "<pre>" . PHP_EOL;
+echo "index /" . PHP_EOL;
+echo "=========" . PHP_EOL;
+
+var_dump($_SERVER);
+
+echo "</pre>" . PHP_EOL;
